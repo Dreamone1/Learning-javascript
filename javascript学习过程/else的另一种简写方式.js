8d501ec065f60ac71d423044fe677dbbfeb46533
我不会github <a531924361@gmail.com>
2018-08-27 23:58:25 +0800

    标题有写

---
+++ javascript学习过程/else的另一种简写方式.js
@@ -0,0 +1,16 @@
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+let age = 16;
+
+age >= 16 ? console.log('You are old enough to drive in the United States!') : console.log('You are not old enough to drive in the United States!');
