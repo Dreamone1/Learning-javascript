049fd6b608b1571b785b172ffafeb06460b0b00b
我不会github <a531924361@gmail.com>
2018-08-28 22:36:25 +0800

    三种函数表达式（简单实例）

---
+++ javascript学习过程/三种函数表达式.js
@@ -0,0 +1,18 @@
+//函数三种表达式，功能一模一样
+function isGreaterThan(numberOne, numberTwo){
+    if(numberOne > numberTwo){
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  
+  const square = function (number) {
+    return number * number;
+  };
+  
+  
+  const square = (number) => {
+    return number * number;
+  };
